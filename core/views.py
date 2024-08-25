b5f42a67da1a81f68aff5f0dd9a9dfd4a9f5ecdc
Kamalesh <kamalesh.s7316@gmail.com>
2024-08-26 00:08:25 +0530

    started working on product details

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from django.db.models import Count
 
 # Create your views here.
 def index(request):
-    featured_products = Item.objects.filter(is_sold=False).order_by('-id')[:5]  # Example: Get the 5 most recent unsold items
-    return render(request, 'index.html', {'featured_products': featured_products})
+    featured_products = Item.objects.filter(is_sold=False).order_by('-id')[:4]  # Example: Get the 5 most recent unsold items
+    top_categories = Category.objects.annotate(num_items=Count('items')).order_by('-num_items')[:4]
+    return render(request, 'index.html', {
+        'featured_products': featured_products,
+        'top_categories': top_categories,
+    })
 
 def contact(request):
     return render(request, 'contact.html')
